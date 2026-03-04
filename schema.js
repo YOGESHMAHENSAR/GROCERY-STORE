ce4a6dafd247689bab75af9cf591fcbe3ed96040
Yogesh Singh <yogajm8@gmail.com>
2026-03-04 20:39:38 +0530

    UI corrected and Price type to text

--- schema.js
+++ schema.js
@@ -6,7 +6,7 @@ module.exports.listingSchema = joi.object({
                 title: joi.string().required(),
                 description: joi.string().required(),
                 location: joi.string().required(),
-                price: joi.number().required().min(0),
+                price: joi.string().required().min(0),
                 image: joi.string().allow("",null),  
         }).required()
 })
